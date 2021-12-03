ff50a672e58eb941ee00af24bbef222e1922c537
Dan Higdon <hdan@alumni.utexas.net>
2021-12-03 12:49:07 -0600

    A better, faster, and simpler implementation of the same algorithm.

--- day3_part2.c
+++ day3_part2.c
@@ -1,142 +1,140 @@
 /* Day 3, part 2 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
 
 // Room for 1024 lines of up to 8 digits
 #define LINE_WIDTH 16 
 #define LINE_MAX 4096
-typedef char TLine[LINE_WIDTH];
-typedef TLine TData[LINE_MAX];
+
+typedef char TEntry[LINE_WIDTH];
+typedef TEntry TData[LINE_MAX];
 
 TData data;
 
+// Could dynamically allocate these to match data
+int zeroes[LINE_WIDTH];
+int ones[LINE_WIDTH];
+
 int candidates[LINE_MAX];
-int filtered[LINE_MAX];
 
-int zeroes[ LINE_WIDTH ];
-int ones[ LINE_WIDTH ];
+//------------------------------------------------------------------------------
 
-int tabulate(int count)
+// Count zeroes and ones in the candidate entries
+void tabulate(int count)
 {
    // Zero the counts
-   for (int digit = 0; digit < LINE_WIDTH; ++digit)
-      ones[digit] = zeroes[digit] = 0;
+   for (int i = 0; i < LINE_WIDTH; ++i)
+      ones[i] = zeroes[i] = 0;
 
-   // Scan each line, counting zeroes and ones.
-   int bits = 0;
+   // Scan each candidate in entry, counting zeroes and ones.
    for (int i = 0; i < count; ++i)
    {
-      char * line = data[candidates[i]];
-      for (int digit = 0; line[ digit ]; ++digit)
+      char * entry = data[candidates[i] ];
+      for (int digit = 0; entry[digit]; ++digit)
       {
-         switch( line[ digit ] )
+         switch (entry[digit])
          {
          case '0': ++zeroes[digit]; break;
          case '1': ++ones[digit];   break;
          }
-         bits = digit+1;
       }
    }
-
-   // bits will be the length of the longest line
-   return bits;
 }
 
-int cvt(char * bits)
+//------------------------------------------------------------------------------
+
+// Remove elements of 'candidates' that do not match the criteria.
+// Return the number of candidates remaining.
+int filter(int count, int digit, char select)
 {
-   int result = 0;
-   for (char * p = bits; *p; ++p)
+   printf("Scan digit %d for %c - ", digit, select);
+
+   int read = 0, write = 0;
+   while (read < count)
    {
-      int digit = (*p == '1') ? 1 : 0;
-      result = (result << 1) | digit;
+      int ci = candidates[read++];
+
+      if (data[ci][digit] == select)
+         candidates[write++] = ci;
    }
-   return result;
+
+   printf("%d candidates\n", write);
+
+   return write;
 }
 
+//------------------------------------------------------------------------------
 
-int main( int argc, char** argv )
+int main(int argc, char** argv)
 {
    if (argc != 2) return 1;
 
    FILE * fp = fopen(argv[1], "r");
-   if ( !fp ) return 1;
+   if (!fp) return 1;
+
+   // -------------------------
 
    // Read the data
    int lines = 0;
-   int bits = 0;
+   int digits = 0;
    while (!feof(fp))
    {
-      char * line = data[lines];
-      if (!fgets( line, LINE_WIDTH, fp ))
+      char * entry = data[lines];
+      if (!fgets(entry, LINE_WIDTH, fp))
          break;
 
-      char * nl = strchr(line, '\n');
+      // fgets leave the newline. Remove it
+      char * nl = strchr(entry, '\n');
       if (nl) *nl = '\0';
+      digits = nl - entry;
 
       ++lines;
    }
-   fclose( fp );
+   fclose(fp);
 
-   int cc;
+   // -------------------------
+
+   int cc; // candidate count
+
    // All numbers are candidates at first
    for (cc = 0; cc < lines; ++cc)
       candidates[cc] = cc;
-   bits = tabulate(cc);
 
-   // Run through each bit, filtering down
-   for (int bit = 0; bit < bits && cc > 1; ++bit)
+   // Run through each digit, filtering down
+   for (int digit = 0; digit < digits && cc > 1; ++digit)
    {
+      tabulate(cc);
       // Oxygen takes MOST common digit, ties go to 1
-      char filter = (ones[bit] >= zeroes[bit]) ? '1' : '0';
-      int fc = 0;
-      for (int c = 0; c < cc; ++c)
-      {
-         int ci = candidates[c];
-         if (data[ci][bit] == filter)
-            filtered[fc++] = ci;
-      }
-      printf("Scan bit %d for %c - %d candidates\n",bit, filter, fc);
-
-      // Move filtered into candidates and re-tabulate the ones and zeroes
-      for (cc = 0; cc < fc; ++cc)
-         candidates[cc] = filtered[cc];
-      bits = tabulate(cc);
+      const char select = (ones[digit] >= zeroes[digit]) ? '1' : '0';
+      cc = filter(cc, digit, select);
    }
+
    char * oxygen = data[candidates[0]];
-   int oxy = cvt(oxygen);
+   int oxy = strtoul(oxygen, NULL, 2);
    printf("Oxygen = %s (%d)\n", oxygen, oxy);
 
+   // -------------------------
 
    // For the scrubber, again consider all lines
    for (cc = 0; cc < lines; ++cc)
       candidates[cc] = cc;
-   bits = tabulate(cc);
 
-   for (int bit = 0; bit < bits && cc > 1; ++bit)
+   for (int digit = 0; digit < digits && cc > 1; ++digit)
    {
+      tabulate(cc);
       // Scrubber takes LEAST common digit, ties go to 0
-      char filter = (ones[bit] < zeroes[bit]) ? '1' : '0';
-      int fc = 0;
-      for (int c = 0; c < cc; ++c)
-      {
-         int ci = candidates[c];
-         if (data[ci][bit] == filter)
-            filtered[fc++] = ci;
-      }
-      printf("Scan bit %d for %c - %d candidates\n",bit, filter, fc);
-      // Move filtered into candidates
-      for (cc = 0; cc < fc; ++cc)
-         candidates[cc] = filtered[cc];
-      bits = tabulate(cc);
+      const char select = (ones[digit] < zeroes[digit]) ? '1' : '0';
+      cc = filter(cc, digit, select);
    }
 
    char * scrubber = data[candidates[0]];
-   int scrub = cvt(scrubber);
+   int scrub = strtoul(scrubber, NULL, 2);
    printf("Scrubber = %s (%d)\n", scrubber, scrub);
 
    printf("LR Rating = %d\n", scrub * oxy);
 
    return 0;
 }
+
